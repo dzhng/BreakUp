353ee623bec5d46b6daffa667a68242378185b8a
Team Simply <team@simply.io>
2012-04-09 16:16:22 -0700

    fixed layer prob

--- breakup.js
+++ breakup.js
@@ -11,7 +11,7 @@ _ie_background.style.background = "rgba(0,0,0,0.8)";
 document.body.appendChild(_ie_background); 
 
 var _ie_div = document.createElement("DIV");
-document.body.appendChild(_ie_div); 
+_ie_background.appendChild(_ie_div); 
 _ie_div.appendChild(_ie_frame);
 
 _ie_frame.setAttribute("src", "breakup.html"); 
@@ -23,8 +23,10 @@ _ie_frame.scrolling = "0";
 _ie_div.style.position = "absolute";
 _ie_div.style.top = "20%";
 _ie_div.style.left = "50%";
+_ie_div.style.width = "280px";
 _ie_div.style.marginLeft = "-320px";
 _ie_div.style.background = "white";
 _ie_div.style.border = "5px solid";
 _ie_div.style.borderColor = "rgb(11,72,107)";
+
 
